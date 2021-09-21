caa9343733e48eee4f0ca534422f1114fab0d039
ryliespriggs <rspriggs812@gmail.com>
2021-09-21 16:58:10 -0700

    commit tree 3ce0a89294cc27dc34235549b406430561a3de96

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
-  //add static methods here
+  static capitalize(letter) {
+    return letter.charAt(0).toUpperCase() + letter.slice(1)
+  }
+  static sanitize(characters) {
+    return characters.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize(string) {
+    const except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return string.split(" ").map((words, i) => except.includes(words) && i !== 0 ? words : words.charAt(0).toUpperCase() + words.slice(1)).join(" ")
+  }
+  //the, a, an, but, of, and, for, at, by, and from
+
 }
